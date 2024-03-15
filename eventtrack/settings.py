62f9eff3d618b124e408ce1a1aa7382797b6ff10
Stogov Egor <severeff@gmail.com>
2024-03-15 02:50:47 +0100

    UPD

--- eventtrack/settings.py
+++ eventtrack/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'eventtrack/media')
+STATIC_ROOT = BASE_DIR / 'eventtrack/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

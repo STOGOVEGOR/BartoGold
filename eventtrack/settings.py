b000571dc548113427ea51d2eb69a574add84659
Stogov Egor <severeff@gmail.com>
2024-03-17 14:00:24 +0100

    UPD

--- eventtrack/settings.py
+++ eventtrack/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'  # 'eventtrack/static/'
+STATIC_URL = '/static/'  # 'eventtrack/static/'
 # MEDIA_URL = 'eventtrack/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'eventtrack/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/eventtrack/media')
 # STATIC_ROOT = BASE_DIR / '/eventtrack/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "eventtrack/static/",
